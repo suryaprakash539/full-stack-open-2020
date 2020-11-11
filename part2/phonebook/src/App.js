2608f18bd152d41e065585a4a4409074a4a08a12
suryaprakash <surya.zvky@gmail.com>
2020-11-11 10:51:39 +0530

    2.8 phonebook(made number property added)

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,32 +1,39 @@
 import React,{useState} from 'react'
 
 const App =()=>{
-    const [persons,setPersons] = useState([
-         {name:'Arto Hellas'}
-    ])
+    const [persons,setPersons] = useState([])
+
+    const [newNumber,setNewNumber] = useState('')
 
     const [newName,setNewName] = useState('')
 
     const handleFormSubmit=(event)=>{
         event.preventDefault()
          
-         for(let i=0;i<persons.length;i++){
-             if(persons[i].name===newName){
-                 window.alert(`${newName} is already added to phonebook`)
-                 setNewName('')
-                 return;
-             }
-         }
+
+    const existingPerson = persons.some(person=>person.name===newName)
+    if(existingPerson){
+        window.alert(`${newName} is already added to phonebook`)
+            setNewName('')
+            return;
+    }
+        
         const personObject={
-            name:newName
+            name:newName,
+            number:newNumber
         }
        setPersons(persons.concat(personObject)) 
        setNewName('')
+       setNewNumber('')
     }
 
     const handleNameChange=(event)=>{
       // console.log(event.target.value)
        setNewName(event.target.value)
+    }
+
+    const handleNumberChange=(event)=>{
+       setNewNumber(event.target.value)
     }
 
     return(
@@ -40,11 +47,16 @@ const App =()=>{
                  onChange={handleNameChange}/>
            </div>
            <div>
+          Number:<input
+                 value={newNumber}
+                 onChange={handleNumberChange}/>
+          </div> 
+           <div>
                <button type='submit'>save</button>
            </div>
        </form>
        <h2>Numbers</h2>
-       {persons.map(person=><h3 key={person.name}>{person.name}</h3>)}
+       {persons.map(person=><h3 key={person.name}>{person.name} {person.number}</h3>)}
       </>
     )
  }
